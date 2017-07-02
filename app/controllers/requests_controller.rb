4fe0ea3c366a830bbb86bb17ca0542caa3116a74
Andrew <ajoeford@gmail.com>
2017-07-01 17:53:28 -0700

    added form buttons on request show page that allow dummy uploads (changes status to client submitted) and close request (sets status to closed)

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -19,6 +19,15 @@ class RequestsController < ApplicationController
     @request = Request.find(params[:id])
   end
 
+  def update
+    @request = Request.find(params[:id])
+    if @request.update(request_params)
+      redirect_to project_request_path(@request.project, @request)
+    else
+      render :show
+    end
+  end
+
   private
 
   def request_params
