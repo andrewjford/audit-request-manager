706c96b84b95595c7589aa40e5fccc612e7209ff
Andrew <ajoeford@gmail.com>
2017-07-03 13:21:22 -0700

    refactored request show page view

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,28 @@
 module RequestsHelper
+  def created_at(request)
+    request.created_at.strftime("%m/%d/%Y")
+  end
+
+  def updated_at(request)
+    request.updated_at.strftime("%b %e, %Y %l:%M %p")
+  end
+
+  def status_button(request, action)
+    if !current_user.client?
+      form_tag "/projects/#{params[:project_id]}/requests/#{params[:id]}",
+      method: :patch, class: "form-button" do
+        hidden_field_tag('request[status]', "#{action}") +
+        submit_tag("#{action.chomp('d')} Request", class: "button-link button-in-site")
+      end
+    end
+  end
+
+  def upload_button(request)
+    form_tag "/projects/#{params[:project_id]}/requests/#{params[:id]}",
+      method: :patch, class: "form-button" do
+        hidden_field_tag('request[status]', "Client Submitted") +
+        submit_tag('Upload File', class: "button-link button-in-site")
+      end
+  end
+
 end
