1a3f2b755cc6632fb316af6a1fc2db52a0b25f48
AJ <ajoeford@gmail.com>
2017-07-06 16:16:54 -0700

    added link to users view for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       content_tag(:li, link_to("Organizations", organizations_path))
     end
   end
+
+  def users_link
+    if current_user.admin?
+      content_tag(:li, link_to("Users", users_path))
+    end
+  end
 end
