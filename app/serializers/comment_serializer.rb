eda55b0b2286736618e813603b68c3e975edcf6b
AJ <ajoeford@gmail.com>
2017-07-19 12:34:44 -0700

    request json response looks good now

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :created_at
 
-  belongs_to :request
   belongs_to :user
 end
--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,8 +1,34 @@
 class RequestSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :status, :request_number, :created_at,
-  :updated_at
+    :updated_at,
+    :user,
+    :comments
 
   belongs_to :project
   belongs_to :user
   has_many :comments
+
+  def user
+    {
+      id: object.user.id,
+      organization_id: object.user.organization_id,
+      name: object.user.name,
+      email: object.user.email
+    }
+  end
+
+  def comments
+    object.comments.map do |comment|
+      {
+        id: comment.id,
+        content: comment.content,
+        created_at: comment.created_at,
+        updated_at: comment.updated_at,
+        user: {
+          id: comment.user.id,
+          name: comment.user.name
+        }
+      }
+    end
+  end
 end
