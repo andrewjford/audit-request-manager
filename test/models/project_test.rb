6276447fb5e1ed6c42769c524d7c5aaec2eedd39
AJ <ajoeford@gmail.com>
2017-08-22 20:56:24 -0700

    added model test for project model validations

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save project without title" do
+    project = Project.new
+    assert_not project.save, "Saved the project without a title"
+  end
+
+  test "should not save project if duplicate title" do
+    project1 = Project.new(title: "State 1")
+    project2 = Project.new(title: "State 1")
+    project1.save
+    assert_not project2.save, "Saved project with duplicate name"
+  end
 end
