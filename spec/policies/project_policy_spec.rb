49cdb2adff17a214791471ca34835c650fd090b3
AJ <ajoeford@gmail.com>
2017-09-05 14:45:01 -0700

    added pundit policy tests

---
+++ spec/policies/project_policy_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+require 'pundit/rspec'
+
+describe ProjectPolicy do
+  subject { ProjectPolicy }
+
+  permissions :new?, :create? do
+    it "denies access if not manager/admin" do
+      expect(subject).not_to permit(User.new(role: 2), Project.new())
+      expect(subject).not_to permit(User.new(role: 3), Project.new())
+    end
+
+    it "grants access if user is a manager" do
+      expect(subject).to permit(FactoryGirl.create(:user), Project.create(title: "Test1"))
+    end
+  end
+
+  permissions :show? do
+    it "denies access if not assigned to project" do
+      expect(subject).not_to permit(User.new(role: 1), Project.new())
+      expect(subject).not_to permit(User.new(role: 2), Project.new())
+      expect(subject).not_to permit(User.new(role: 3), Project.new())
+    end
+
+    it "grants access if user is assigned" do
+      @user = FactoryGirl.create(:user)
+
+      expect(subject).to permit(@user,
+        Project.create(title: "Test1", user_ids: [@user.id]))
+    end
+  end
+
+  permissions :edit?, :update? do
+    it "denies access if not admin or assigned manager" do
+      expect(subject).not_to permit(User.new(role: 2), Project.new())
+      expect(subject).not_to permit(User.new(role: 3), Project.new())
+    end
+
+    it "grants access if user is an assigned manager" do
+      @user = FactoryGirl.create(:user)
+
+      expect(subject).to permit(@user,
+        Project.create(title: "Test1", user_ids: [@user.id]))
+
+    end
+  end
+end
